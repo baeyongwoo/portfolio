56382df707cb56393e6205ecc214742bf31ce394
DESKTOP-NVPT6OO\dyddn <dyddn30612@naver.com>
2024-12-13 20:31:25 +0900

    맨위로 버튼과 프로젝트 2,3완료

--- pj02/script/script.js
+++ pj02/script/script.js
@@ -20,3 +20,26 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
+
+
+
+function topFunction() {
+  window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+  });
+}
+
+
+window.onscroll = function() {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  const topBtn = document.getElementById("topBtn");
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      topBtn.style.display = "block"; 
+  } else {
+      topBtn.style.display = "none"; 
+  }
+}
